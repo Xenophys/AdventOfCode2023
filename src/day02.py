0fab063e1d78adb3d54f8a95825c5338f26c32b2
Thorsten Baumann <thorsten@baumann-sb.de>
2023-12-03 18:29:01 +0100

    Add solution for day 02

---
+++ src/day02.py
@@ -0,0 +1,121 @@
+# Day02
+from src.utils import readfile
+
+
+class CubeSet:
+    def __init__(self, blue=0, green=0, red=0):
+        self.blue = blue
+        self.green = green
+        self.red = red
+
+    def set_cubes(self, count, color):
+        number = int(count)
+        match color:
+            case "blue":
+                self.blue = number
+            case "green":
+                self.green = number
+            case "red":
+                self.red = number
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            print("CubeSet type mismatch")
+            return False
+        if self.blue != other.blue:
+            print(f"blue mismatch: {str(self.blue)} vs {str(other.blue)}")
+            return False
+        if self.green != other.green:
+            print(f"green mismatch: {str(self.green)} vs {str(other.green)}")
+            return False
+        if self.red != other.red:
+            print(f"red mismatch: {str(self.red)} vs {str(other.red)}")
+            return False
+        return True
+
+class Game:
+    def __init__(self, game_number: int):
+        self.game_number = game_number
+        self.cube_sets = []
+
+    def add_cube_set(self, cube_set: CubeSet):
+        self.cube_sets.append(cube_set)
+
+    def is_possible(self, check_cube_set: CubeSet) -> bool:
+        for cube_set in self.cube_sets:
+            if cube_set.blue > check_cube_set.blue:
+                return False
+            if cube_set.green > check_cube_set.green:
+                return False
+            if cube_set.red > check_cube_set.red:
+                return False
+        return True
+
+    def get_power(self):
+        required_blue = 0
+        required_green = 0
+        required_red = 0
+        for cube_set in self.cube_sets:
+            if cube_set.blue > required_blue:
+                required_blue = cube_set.blue
+            if cube_set.green > required_green:
+                required_green = cube_set.green
+            if cube_set.red > required_red:
+                required_red = cube_set.red
+        return required_blue * required_green * required_red
+
+    def __eq__(self, other):
+        if not isinstance(other, type(self)):
+            print("Type mismatch")
+            return False
+        if not self.game_number == other.game_number:
+            print("Game number mismatch")
+            return False
+        if len(self.cube_sets) != len(other.cube_sets):
+            print(f"CubeSet length mismatch {str(len(self.cube_sets))} vs. {str(len(other.cube_sets))}")
+            return False
+        for index in range(0, len(self.cube_sets)):
+            if self.cube_sets[index] != other.cube_sets[index]:
+                print(f"CubeSet mismatch {str(self.cube_sets[index])} vs. {str(other.cube_sets[index])}")
+                return False
+        return True
+
+
+def parse_game(line: str) -> Game:
+    split_line = line.split(":")
+    game_number = int(split_line[0].split(" ")[1])
+    game = Game(game_number)
+    for cube_set_string in split_line[1].split(";"):
+        cube_set = CubeSet()
+        for cubes in cube_set_string.split(","):
+            split_cubes = cubes.strip().split(" ")
+            cube_set.set_cubes(split_cubes[0], split_cubes[1])
+        game.add_cube_set(cube_set)
+    return game
+
+
+def part01(input_file_name: str, cubes_to_check: CubeSet):
+    lines = readfile(input_file_name)
+    possible_game_numbers = []
+    for line in lines:
+        game = parse_game(line)
+        if game.is_possible(cubes_to_check):
+            possible_game_numbers.append(game.game_number)
+    return sum(possible_game_numbers)
+
+
+def part02(input_file_name: str):
+    lines = readfile(input_file_name)
+    game_powers = []
+    for line in lines:
+        game = parse_game(line)
+        game_powers.append(game.get_power())
+    return sum(game_powers)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    part1_result = part01("resources/input_day02.txt", CubeSet(red=12, green=13, blue=14))
+    print(f"Result of part 1: '{str(part1_result)}'")
+    part2_result = part02("resources/input_day02.txt")
+    print(f"Result of part 2: '{str(part2_result)}'")
---
+++ tests/test_day02.py
@@ -0,0 +1,118 @@
+from src.day02 import Game, CubeSet, parse_game, part01, part02
+from src.utils import readfile
+
+
+
+def build_expected_games():
+    games = []
+    # Game 1
+    game = Game(1)
+    cube_set = CubeSet()
+    cube_set.set_cubes("3", "blue")
+    cube_set.set_cubes("4", "red")
+    game.add_cube_set(cube_set)
+
+    cube_set = CubeSet()
+    cube_set.set_cubes("1", "red")
+    cube_set.set_cubes("2", "green")
+    cube_set.set_cubes("6", "blue")
+    game.add_cube_set(cube_set)
+
+    cube_set = CubeSet()
+    cube_set.set_cubes("2", "green")
+    game.add_cube_set(cube_set)
+    games.append(game)
+
+    #  Game 2
+    game = Game(2)
+    cube_set = CubeSet()
+    cube_set.set_cubes("1", "blue")
+    cube_set.set_cubes("2", "green")
+    game.add_cube_set(cube_set)
+
+    cube_set = CubeSet()
+    cube_set.set_cubes("3", "green")
+    cube_set.set_cubes("4", "blue")
+    cube_set.set_cubes("1", "red")
+    game.add_cube_set(cube_set)
+
+    cube_set = CubeSet()
+    cube_set.set_cubes("1", "green")
+    cube_set.set_cubes("1", "blue")
+    game.add_cube_set(cube_set)
+    games.append(game)
+
+    game = Game(3)
+    cube_set = CubeSet()
+    cube_set.set_cubes("8", "green")
+    cube_set.set_cubes("6", "blue")
+    cube_set.set_cubes("20", "red")
+    game.add_cube_set(cube_set)
+
+    cube_set = CubeSet()
+    cube_set.set_cubes("5", "blue")
+    cube_set.set_cubes("4", "red")
+    cube_set.set_cubes("13", "green")
+    game.add_cube_set(cube_set)
+
+    cube_set = CubeSet()
+    cube_set.set_cubes("5", "green")
+    cube_set.set_cubes("1", "red")
+    game.add_cube_set(cube_set)
+    games.append(game)
+
+    game = Game(4)
+    cube_set = CubeSet()
+    cube_set.set_cubes("1", "green")
+    cube_set.set_cubes("3", "red")
+    cube_set.set_cubes("6", "blue")
+    game.add_cube_set(cube_set)
+
+    cube_set = CubeSet()
+    cube_set.set_cubes("3", "green")
+    cube_set.set_cubes("6", "red")
+    game.add_cube_set(cube_set)
+
+    cube_set = CubeSet()
+    cube_set.set_cubes("3", "green")
+    cube_set.set_cubes("15", "blue")
+    cube_set.set_cubes("14", "red")
+    game.add_cube_set(cube_set)
+    games.append(game)
+
+    game = Game(5)
+    cube_set = CubeSet()
+    cube_set.set_cubes("6", "red")
+    cube_set.set_cubes("1", "blue")
+    cube_set.set_cubes("3", "green")
+    game.add_cube_set(cube_set)
+
+    cube_set = CubeSet()
+    cube_set.set_cubes("2", "blue")
+    cube_set.set_cubes("1", "red")
+    cube_set.set_cubes("2", "green")
+    game.add_cube_set(cube_set)
+
+    games.append(game)
+    return games
+
+
+
+def test_parse_game():
+    input = readfile("resources/test-input_day02_01.txt")
+    expected = build_expected_games()
+    for index in range(0, len(input)):
+        game = parse_game(input[index])
+        expected_game = expected[index]
+        assert game == expected_game
+
+
+def test_part01():
+    test_cube_set = CubeSet(red=12, green=13, blue=14)
+    solution = part01("resources/test-input_day02_01.txt", test_cube_set)
+    assert solution == 8
+
+
+def test_part02():
+    solution = part02("resources/test-input_day02_01.txt")
+    assert solution == 2286
