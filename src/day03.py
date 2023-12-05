5094935a9ae61ce37b03261efd86ce3edba032a2
Thorsten Baumann <thb@baumann-sb.de>
2023-12-05 08:06:13 +0100

    Add solution for day03 part 1

--- src/day03.py
+++ src/day03.py
@@ -1,8 +1,54 @@
-def part01(input_file_name: str):
-    pass
+from src import utils
 
+def is_marker(char:str)->bool:
+    return char != "." and not char.isnumeric()
+
+
+def number_has_marker(row_index: int, col_index: int, lines: list[str]):
+    for row in range(max(0, row_index - 1), min(row_index + 2, len(lines))):
+        line = lines[row]
+        for column in range(max(0, col_index - 1), min(col_index + 2, len(line))):
+            if is_marker(line[column]):
+                return True
+    return False
+
+
+def extract_marked_numbers(lines: list[str]):
+    numbers: list[int] = []
+    actual_number = 0
+    keep_number = False
+    for line_index in range(0, len(lines)):
+        line = lines[line_index]
+        for column_index in range(0, len(line)):
+            actual_char = str(line[column_index])
+            if actual_char.isnumeric():
+                actual_number = actual_number * 10 + int(line[column_index])
+                if not keep_number:
+                    keep_number = number_has_marker(line_index, column_index, lines)
+            elif is_marker(actual_char):
+                if actual_number > 0:
+                    numbers.append(actual_number)
+                actual_number = 0
+                keep_number = False
+            else:
+                if keep_number:
+                    numbers.append(actual_number)
+                actual_number = 0
+                keep_number = False
+        if actual_number > 0 and keep_number:
+            numbers.append(actual_number)
+        actual_number = 0
+        keep_number = False
+    return numbers
+
+
+def part01(input_file_name: str):
+    lines = utils.readfile(input_file_name)
+    numbers = extract_marked_numbers(lines)
+    return sum(numbers)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    part01("resources/input_day03.txt")
+    part01_result = part01("resources/input_day03.txt")
+    print(f"Result of part 1: '{str(part01_result)}'")
---
+++ tests/test_day03.py
@@ -0,0 +1,14 @@
+from src.day03 import extract_marked_numbers, part01
+from src.utils import readfile
+
+expected_numbers = [467, 35, 633, 617, 592, 755, 664, 598]
+
+
+def test_extract_marked_numbers():
+    lines = readfile("resources/test-input_day03.txt")
+    numbers = extract_marked_numbers(lines)
+    assert numbers == expected_numbers
+
+def test_part01():
+    result = part01("resources/test-input_day03.txt")
+    assert result == 4361
