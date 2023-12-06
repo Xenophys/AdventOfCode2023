c5d4f09181776e4b8d4a1c737f3f1a0cddcd4e47
Thorsten Baumann <thorsten@baumann-sb.de>
2023-12-06 21:31:12 +0100

    Add solution for day03 part 2

--- day03/day03.py
+++ day03/day03.py
@@ -43,13 +43,61 @@ def extract_marked_numbers(lines: list[str]):
     return numbers
 
 
+def check_and_add_number(lines, number: int, pos_x: int, pos_y:int, star_dict):
+    positions = []
+    for row in range(max(0, pos_y - 1), min(pos_y + 2, len(lines))):
+        line = lines[row]
+        for column in range(max(0, pos_x - len(str(number)) - 1), min(pos_x + 1, len(line))):
+            if line[column] == "*":
+                position = f"{column},{row}"
+                positions.append(position)
+    if len(positions) > 0:
+        for position in positions:
+            num_list = star_dict[position] if star_dict.get(position) else []
+            num_list.append(number)
+            star_dict[position] = num_list
+    return star_dict
+
+
+def extract_gear_ratios(lines: list[str]):
+    gears = {}
+    actual_number = 0
+    for line_index in range(0, len(lines)):
+        line = lines[line_index]
+        for column_index in range(0, len(line)):
+            actual_char = str(line[column_index])
+            if actual_char.isnumeric():
+                actual_number = actual_number * 10 + int(line[column_index])
+            else:
+                if actual_number > 0:
+                    gears = check_and_add_number(lines, actual_number, column_index, line_index, gears)
+                actual_number = 0
+        if actual_number > 0:
+            gears = check_and_add_number(lines, actual_number, len(line), line_index, gears)
+        actual_number = 0
+    gear_ratios = []
+    for key in gears:
+        numbers = gears.get(key)
+        if len(numbers) == 2:
+            gear_ratios.append(numbers[0] * numbers[1])
+    return gear_ratios
+
+
 def part01(input_file_name: str):
     lines = readfile(input_file_name)
     numbers = extract_marked_numbers(lines)
     return sum(numbers)
 
 
+def part02(input_file_name: str):
+    lines = readfile(input_file_name)
+    gear_ratios = extract_gear_ratios(lines)
+    return sum(gear_ratios)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     part01_result = part01("input_day03.txt")
     print(f"Result of part 1: '{str(part01_result)}'")
+    part02_result = part02("input_day03.txt")
+    print(f"Result of part 2: '{str(part02_result)}'")
--- day03/test_day03.py
+++ day03/test_day03.py
@@ -1,5 +1,5 @@
-from .day03 import extract_marked_numbers, part01
 from utils.utils import readfile
+from .day03 import extract_marked_numbers, part01, extract_gear_ratios, part02
 
 expected_numbers = [467, 35, 633, 617, 592, 755, 664, 598]
 
@@ -13,3 +13,14 @@ def test_extract_marked_numbers():
 def test_part01():
     result = part01("day03/test-input_day03.txt")
     assert result == 4361
+
+
+def test_extract_numbers_with_star():
+    lines = readfile("day03/test-input_day03.txt")
+    result = extract_gear_ratios(lines)
+    assert result == [16345,451490]
+
+
+def test_part02():
+    result = part02("day03/test-input_day03.txt")
+    assert result == 467835
